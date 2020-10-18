f3df4ecafa835b456c5625f9877fa979b038d08c
blblemos <blblemos17@gmail.com>
2020-10-18 00:42:17 -0300

    Finalizando dia 2

---
+++ src/models/Images.ts
@@ -0,0 +1,15 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
+import Orphanege from './Orphanage';
+
+@Entity('images')
+export default class Image {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column()
+    path: string;
+    
+    @ManyToOne(() => Orphanege, orphanage => orphanage.images)
+    @JoinColumn({name: 'orphanage_id'})
+    orphanage: Orphanege
+}
